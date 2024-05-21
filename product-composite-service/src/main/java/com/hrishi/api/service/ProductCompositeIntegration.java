97fbff22c13b6dd3153684c4b72e1f520c2df14f
Hrishi Kesav <hrishikesav@gmail.com>
2024-05-21 06:48:43 +0530

    Adding centralized exception handling.

--- product-composite-service/src/main/java/com/hrishi/api/service/ProductCompositeIntegration.java
+++ product-composite-service/src/main/java/com/hrishi/api/service/ProductCompositeIntegration.java
@@ -1,11 +1,17 @@
 package com.hrishi.api.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hrishi.api.exceptions.HttpErrorInfo;
+import com.hrishi.api.exceptions.InvalidInputException;
+import com.hrishi.api.exceptions.NotFoundException;
 import com.hrishi.api.model.Product;
 import com.hrishi.api.model.Recommendation;
 import com.hrishi.api.model.Review;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
@@ -20,6 +26,9 @@ import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
 @Component
 public class ProductCompositeIntegration implements ProductService, RecommendationService, ReviewService{
+
+    private static final Logger LOG = LoggerFactory.getLogger(ProductCompositeIntegration.class);
+
 
     RestTemplate restTemplate;
     ObjectMapper mapper;
@@ -49,7 +58,19 @@ public class ProductCompositeIntegration implements ProductService, Recommendati
             return restTemplate.getForObject(url, Product.class);
 
         } catch (HttpClientErrorException ex) {
-            throw ex;
+            switch (HttpStatus.valueOf(ex.getStatusCode().value())) {
+
+                case NOT_FOUND:
+                    throw new NotFoundException(getErrorMessage(ex));
+
+                case UNPROCESSABLE_ENTITY :
+                    throw new InvalidInputException(getErrorMessage(ex));
+
+                default:
+                    LOG.warn("Got a unexpected HTTP error: {}, will rethrow it", ex.getStatusCode());
+                    LOG.warn("Error body: {}", ex.getResponseBodyAsString());
+                    throw ex;
+            }
         }
     }
 
@@ -78,6 +99,14 @@ public class ProductCompositeIntegration implements ProductService, Recommendati
 
         } catch (Exception ex) {
             return new ArrayList<>();
+        }
+    }
+
+    private String getErrorMessage(HttpClientErrorException ex) {
+        try {
+            return mapper.readValue(ex.getResponseBodyAsString(), HttpErrorInfo.class).getMessage();
+        } catch (IOException ioex) {
+            return ex.getMessage();
         }
     }
 
--- product-service/src/main/java/com/hrishi/api/service/ProductServiceImpl.java
+++ product-service/src/main/java/com/hrishi/api/service/ProductServiceImpl.java
@@ -1,15 +1,24 @@
 package com.hrishi.api.service;
 
+import com.hrishi.api.exceptions.InvalidInputException;
 import com.hrishi.api.model.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductServiceImpl implements ProductService{
+
+    private static final Logger LOG = LoggerFactory.getLogger(ProductServiceImpl.class);
     @Autowired
     ServiceUtil serviceUtil;
     @Override
     public Product getProduct(int productId) {
+        LOG.debug("/product return the found product for productId={}", productId);
+
+        if (productId < 1) throw new InvalidInputException("Invalid productId: " + productId);
+
         return new Product(productId, "name-" + productId, 123, serviceUtil.getServiceAddress());
     }
 }
